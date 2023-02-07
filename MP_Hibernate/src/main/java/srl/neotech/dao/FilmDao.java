4455172de4f3640b7b4d13b15757f47d377d5053
MaPaxe <m.pace@neotech.srl>
2023-02-07 14:12:42 +0100

    prima commit hibernate

---
+++ MP_Hibernate/src/main/java/srl/neotech/dao/FilmDao.java
@@ -0,0 +1,47 @@
+package srl.neotech.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.hibernate.criterion.Example;
+import org.hibernate.criterion.Example.PropertySelector;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.stereotype.Component;
+
+import srl.neotech.entity.Language;
+import srl.neotech.entity.Movie;
+import srl.neotech.repository.MovieRepository;
+
+@Component
+public class FilmDao {
+
+	@Autowired
+	EntityManager entityManager;
+	
+	@Autowired
+	MovieRepository movieRepository;
+	
+	public Movie getMovie(Integer movie_id) {
+	
+	//find by PK	
+	Movie m=entityManager.find(Movie.class, movie_id); 	
+	return m;	
+	}
+	
+	public List<Movie> getMovieByActor(String name) {
+		
+		//find by PK	
+		 List<Movie> movies=movieRepository.getFilmByActor(name);
+		return movies;	
+		}
+	
+	
+	public void insertMovie(Movie movie) {
+		
+	//insert
+	entityManager.persist(movie); 
+	}
+	
+}
